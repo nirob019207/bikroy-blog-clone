d9c73f707559a6415093ddb04d96d95304ef53b0
nirob019207 <nirobhasanbd01955@gmial.com>
2024-08-31 02:12:01 -0700

    updated hero part

--- resources/views/frontend/hero.blade.php
+++ resources/views/frontend/hero.blade.php
@@ -3,6 +3,7 @@
         <div class="row row-custom">
             <!-- Big Photo Column -->
             <div class="col-md-6 col-12">
+            <a href="{{ route('singleblog', $latestThree[0]->id) }}">
                 <div class="bigc shadow-lg cardd">
                 <img src="{{ asset('storage/' . $latestThree[0]->image) }}" alt="{{ $latestThree[0]->title }}" class="big-photo py-2">
                     <div class="intro1 container-fluid">
@@ -10,11 +11,13 @@
                     <p>{{ Str::limit($latestThree[0]->short_description, 100) }}</p>
                     </div>
                 </div>
+</a>
             </div>
             <!-- Small Photos Column -->
             <div class="col-md-6 col-12 d-flex cardd flex-md-column flex-row">
             @foreach ($latestThree->slice(1, 2) as $post)
 
+                <a href="{{ route('singleblog', $latestThree[0]->id) }}">
                 <div class="smallc position-relative">
                 <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title }}" class="small-photo1 py-2">
                 <div class="intro2">
@@ -22,6 +25,7 @@
                     <p>{{ Str::limit($latestThree[0]->short_description, 100) }}</p>
                     </div>
                 </div>
+                </a>
                 @endforeach
               
             </div>
