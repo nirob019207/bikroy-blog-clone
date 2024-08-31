93dc527dd27fde8899702d55bee516280ea8d085
nirob019207 <nirobhasanbd01955@gmial.com>
2024-08-31 02:13:13 -0700

    simple change

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap Sticky Navbar Example</title>
+    <title>bikroy blog clone</title>
     <link rel="stylesheet" href="{{asset('frontend/style.css')}}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">    
